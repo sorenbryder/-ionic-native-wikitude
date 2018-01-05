7feec360175c7e83d454b222316233564cb2735c
Søren Bryder Nielsen <soerenb@gmail.com>
2018-01-05 11:24:08 +0100

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,31 @@
+/**
+ * @name Wikitude
+ * @description
+ * This plugin does something
+ *
+ * @usage
+ * ```
+ * import { Wikitude } from 'ionic-native';
+ *
+ *
+ * constructor(private wikitude: Wikitude) { }
+ *
+ * ...
+ *
+ *
+ * this.wikitude.functionName('Hello', 123)
+ *   .then((res: any) => console.log(res))
+ *   .catch((error: any) => console.error(error));
+ *
+ * ```
+ */
+export declare class Wikitude {
+    isDeviceSupported(requiredFeatures: string[]): Promise<any>;
+    requestAccess(requiredFeatures: string[]): Promise<any>;
+    loadARchitectWorld(architectWorldPath: string, requiredFeatures: string[], startupConfiguration: Object): Promise<any>;
+    setOnUrlInvokeCallback(onUrlInvokeCallback: Function): void;
+    callJavaScript(js: string): void;
+    close(): void;
+    hide(): void;
+    show(): void;
+}
